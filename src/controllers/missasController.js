daac9a2f58377c299db7d0a792c2fad31f156b5f
Venicius <venicius.alves@gmail.com>
2020-08-09 12:01:41 -0300

    inclusao de rota para buscar missa por id

--- src/controllers/missasController.js
+++ src/controllers/missasController.js
@@ -1,52 +1,58 @@
 const db = require("../database/connection");
 
-
 async function listMissas(request, response) {
-    const missas = await db("missas");
+  const missas = await db("missas");
 
-    return response.status(200).json(missas);
+  return response.status(200).json(missas);
+}
+
+async function listMissaById(request, response) {
+  const params = request.params;
+  const missas = await db("missas").where("id","=",params.id);
+
+  return response.status(200).json(missas);
+}
+
+async function createMissa(request, response) {
+  const missa = { ...request.body };
+
+  const trx = await db.transaction();
+  try {
+    const insertedMissasIds = await trx("missas").insert(missa);
+    await trx.commit();
+    return response.status(201).json({
+      id: insertedMissasIds,
+    });
+  } catch (error) {
+    console.log(error);
+    await trx.rollback();
+    return response.status(400).json({
+      error: "Erro Inexperado",
+    });
   }
+}
 
-  async function createMissa(request, response) {
-    const missa = { ...request.body };
+async function updateMissa(request, response) {
+  const missa = { ...request.body };
+  const params = request.params;
 
-    const trx = await db.transaction();
-    try {
-      const insertedMissasIds = await trx("missas").insert(missa);
-      await trx.commit();
-      return response.status(201).json({
-        id: insertedMissasIds,
-      });
-    } catch (error) {
-      console.log(error);
-      await trx.rollback();
-      return response.status(400).json({
-        error: "Erro Inexperado",
-      });
-    }
+  const trx = await db.transaction();
+  try {
+    await trx("missas").where("id", "=", params.id).update(missa);
+    await trx.commit();
+    return response.status(201).json({
+      message: "Operação realizada com sucesso!",
+    });
+  } catch (error) {
+    console.log(error);
+    await trx.rollback();
+    return response.status(400).json({
+      error: "Erro Inexperado",
+    });
   }
-
-  async function updateMissa(request, response) {
-    const missa = { ...request.body };
-    const params = request.params;
-
-    const trx = await db.transaction();
-    try {
-      await trx("missas").where("id", "=", params.id).update(missa);
-      await trx.commit();
-      return response.status(201).json({
-        message: "Operação realizada com sucesso!",
-      });
-    } catch (error) {
-      console.log(error);
-      await trx.rollback();
-      return response.status(400).json({
-        error: "Erro Inexperado",
-      });
-    }
-  }
+}
 
 exports.listMissas = listMissas;
 exports.updateMissa = updateMissa;
 exports.createMissa = createMissa;
-
+exports.listMissaById = listMissaById;
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 const MissasController = require("./controllers/missasController");
 const SenhasController = require("./controllers/senhasController");
+
 routes.post("/missas", MissasController.createMissa);
 routes.get("/missas", MissasController.listMissas);
 routes.put("/missas/:id", MissasController.updateMissa);
+routes.get("/missas/:id", MissasController.listMissaById);
 
 routes.post("/senhas", SenhasController.solicitaSenha);
 routes.get("/senhas", SenhasController.listSenhas);
 routes.put("/senhas/:id", SenhasController.updateSenhas);
 
-module.exports = routes; 
+module.exports = routes;
