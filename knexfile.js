e9dca14403cc827119278d3f7e693455bfcb7638
Venicius <venicius.alves@gmail.com>
2020-08-31 22:57:28 -0300

    Configurando o postgress

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,13 @@ module.exports = {
   production: {
     client: "sqlite3",
     connection: {
-      filename: "./src/database/database-prod.sqlite",
+      host: "ec2-54-91-178-234.compute-1.amazonaws.com",
+      user: "moqbbevbskvkfh",
+      password:
+        "0d41d1af53a681433144837379f693ea4cff6cedc5e52a39195d3ae60e050967",
+      database: "d2ht1m9me48g81",
+      uri:
+        "postgres://moqbbevbskvkfh:0d41d1af53a681433144837379f693ea4cff6cedc5e52a39195d3ae60e050967@ec2-54-91-178-234.compute-1.amazonaws.com:5432/d2ht1m9me48g81",
     },
     migrations: {
       directory: "./src/database/migrations",
